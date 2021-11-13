5023d5b070e6c2d59a0181e7a1f6f0b3619c389b
Jeremy Myers <inernestmusic@gmail.com>
2021-11-13 17:07:40 -0500

    Import createWhereElements to render in ViewItinerary

--- src/components/ViewItinerary.js
+++ src/components/ViewItinerary.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import uniqid from "uniqid";
+import React from "react";
+import { createWhereElements } from "../functions/helperDOMMethods";
 
 const ViewItinerary = (props) => {
   //   console.log(props);
@@ -7,10 +7,18 @@ const ViewItinerary = (props) => {
   return (
     <div id="itinerary-container">
       {props.markers.map((object, index) => {
+        const whereElements = createWhereElements(object.place);
         return (
           <div key={object.id} data-id={object.id}>
             <h1>thing {index + 1}</h1>
-            <p>where: {object.place.name}</p>
+            <h2>where</h2>
+            <div id="view-where">
+              {whereElements[0].innerHTML}
+              <br></br>
+              {whereElements[1].innerHTML}
+              <br></br>
+              <a href={whereElements[2].href}>{whereElements[2].innerHTML}</a>
+            </div>
             <p>
               when: {object.userInputData[0].value} @{" "}
               {object.userInputData[1].value}
