2fbb190d1c2d6e5d3ef986aae75e02afec30255a
Jeremy Myers <inernestmusic@gmail.com>
2021-11-17 17:00:29 -0500

    Render connections object from Firestore
    - Restructured connections data in Firestore to help render connections data
    - Added some grid and flex properties for general layout

--- src/components/Connections.js
+++ src/components/Connections.js
@@ -3,12 +3,45 @@ import React from "react";
 
 function Connections(props) {
   console.log(props.userData);
+  const connectionsObject = props.userData.connections;
   return (
     <div id="connections-container">
-      <h1>connections</h1>
-      <h2>your active connections:</h2>
-      <h2>your pending requests:</h2>
-      <h2>your sent requests:</h2>
+      <div id="active-connects-container">
+        <h2 id="active-connects-header">Your Connections</h2>
+        {connectionsObject.active.length ? (
+          connectionsObject.active.map((connect) => {
+            return <p>{connect.name}</p>;
+          })
+        ) : (
+          <p>looking lonely- connect with other adventurers!</p>
+        )}
+      </div>
+      <div id="manage-connects-container">
+        <h2 id="manage-header">Manage Invitations</h2>
+        <div className="pending-connects-container">
+          <h3>Received</h3>
+          {connectionsObject.pendingReceived.map((connect) => {
+            return (
+              <div className="manage-connects">
+                <p>{connect.name}</p>
+                <button>Approve</button>
+                <button>Deny</button>
+              </div>
+            );
+          })}
+        </div>
+        <div className="pending-connects-container">
+          <h3>Sent</h3>
+          {connectionsObject.pendingSent.map((connect) => {
+            return (
+              <div className="manage-connects">
+                <p>{connect.name}</p>
+                <button>Withdraw</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
       <div id="search-connections-container">
         <label htmlFor="search-connections"></label>
         <input
