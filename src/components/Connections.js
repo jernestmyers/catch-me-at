1c8c21b7af731577b06e70a6d9c78a6103753d93
Jeremy Myers <inernestmusic@gmail.com>
2021-11-29 21:13:20 -0500

    Style guest component rendered messages
    - In components like ViewMaps and Connections, guest users are rendered messages in lieu of content

--- src/components/Connections.js
+++ src/components/Connections.js
@@ -287,10 +287,111 @@ function Connections({ db, userData, userAuth, users, setUserData }) {
           </div>
         </div>
       ) : (
-        <div>
-          <h1>
-            Sign in with a Google account to connect with other adventurers!
-          </h1>
+        <div id="guest-container">
+          <h1>Sign in with a Google account to...</h1>
+          <ul id="guest-list">
+            <li className="guest-list-items">
+              <svg className="guest-icon" viewBox="0 0 64 64" role="img">
+                <circle
+                  data-name="layer2"
+                  cx="32"
+                  cy="39"
+                  r="7"
+                  fill="#FFEB3B"
+                  stroke="#202020"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></circle>
+                <path
+                  data-name="layer2"
+                  d="M32 46a12.1 12.1 0 0 0-12 12v2h24v-2a12.1 12.1 0 0 0-12-12z"
+                  fill="#dfdee3"
+                  stroke="#202020"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+                <circle
+                  data-name="layer2"
+                  cx="52"
+                  cy="10"
+                  r="6"
+                  fill="#FFEB3B"
+                  stroke="#202020"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></circle>
+                <path
+                  data-name="layer2"
+                  d="M62 28c0-7.5-4.5-12-10-12s-10 4.5-10 12z"
+                  fill="#bdd5ae"
+                  stroke="#202020"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+                <circle
+                  data-name="layer2"
+                  cx="12"
+                  cy="10"
+                  r="6"
+                  fill="#FFEB3B"
+                  stroke="#202020"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></circle>
+                <path
+                  data-name="layer2"
+                  d="M22 28c0-7.5-4.5-12-10-12S2 20.5 2 28z"
+                  fill="#a2bce0"
+                  stroke="#202020"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+                <path
+                  data-name="layer1"
+                  fill="none"
+                  stroke="#202020"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                  d="M12 34l8 8m32-8l-8 8M24 14h16"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+              </svg>
+              Connect with other adventurers.
+            </li>
+            <li className="guest-list-items">
+              <svg
+                className="guest-icon"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+              >
+                <path
+                  strokeWidth="2"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  stroke="#202020"
+                  fill="#bdd5ae"
+                  d="M32 22V10l28 20-28 20V38c-11.1 0-21.3.4-30 16 0-9.9 1-32 30-32z"
+                  data-name="layer1"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+              Share private maps with connections.
+            </li>
+          </ul>
         </div>
       )}
     </div>
--- src/components/ViewMaps.js
+++ src/components/ViewMaps.js
@@ -100,12 +100,124 @@ function ViewMaps(props) {
           </div>
         </div>
       ) : (
-        <div>
+        <div id="guest-container">
           <h1>Sign in with a Google account to view...</h1>
-          <ul>
-            <li>Maps you create.</li>
-            <li>Public maps you save.</li>
-            <li>Maps shared with you.</li>
+          <ul id="guest-list">
+            <li className="guest-list-items">
+              <svg
+                className="guest-icon"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+              >
+                <path
+                  d="M38.1 46H52l8 16H4l8-16h13.9"
+                  strokeWidth="2"
+                  strokeMiterlimit="10"
+                  stroke="#202020"
+                  fill="#A2BCE0"
+                  data-name="layer2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+                <path
+                  strokeWidth="2"
+                  strokeMiterlimit="10"
+                  stroke="#202020"
+                  fill="red"
+                  d="M32 2a18.1 18.1 0 0 0-18.1 18.1C13.9 36.4 32 52.4 32 52.4s18.1-16 18.1-32.3A18.1 18.1 0 0 0 32 2z"
+                  data-name="layer1"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+                <ellipse
+                  strokeWidth="2"
+                  strokeMiterlimit="10"
+                  stroke="#202020"
+                  fill="#d00000"
+                  ry="6"
+                  rx="6"
+                  cy="20"
+                  cx="32"
+                  data-name="layer1"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></ellipse>
+              </svg>
+              Maps you create.
+            </li>
+            <li className="guest-list-items">
+              <svg
+                className="guest-icon"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 64 64"
+                role="img"
+              >
+                <path
+                  data-name="layer2"
+                  fill={"#a2bce0"}
+                  stroke="#202020"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                  d="M31.2 52H2V2h43.3L52 8.7v22.5"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+                <path
+                  data-name="layer2"
+                  fill="none"
+                  stroke="#202020"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                  d="M44 30.1V28H10v24m24-42v4m8-12v16.3a1.7 1.7 0 0 1-1.7 1.7H23.7a1.7 1.7 0 0 1-1.7-1.7V2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+                <circle
+                  data-name="layer1"
+                  cx="46"
+                  cy="46"
+                  r="16"
+                  fill={"#bdd5ae"}
+                  stroke="#202020"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></circle>
+                <path
+                  data-name="layer1"
+                  fill="none"
+                  stroke="#202020"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                  d="M46 38v16m-8-8h16"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+              </svg>
+              Public maps you save.
+            </li>
+            <li className="guest-list-items">
+              <svg
+                className="guest-icon"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+              >
+                <path
+                  strokeWidth="2"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  stroke="#202020"
+                  fill="#bdd5ae"
+                  d="M32 22V10l28 20-28 20V38c-11.1 0-21.3.4-30 16 0-9.9 1-32 30-32z"
+                  data-name="layer1"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+              Maps shared with you.
+            </li>
           </ul>
         </div>
       )}
