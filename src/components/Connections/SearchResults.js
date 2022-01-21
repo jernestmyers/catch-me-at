50b684575746231aef135dcf3d1bfb0d536abc87
Jeremy Myers <inernestmusic@gmail.com>
2022-01-21 16:51:53 -0500

    Move key from this component to Connections component

--- src/components/Connections/SearchResults.js
+++ src/components/Connections/SearchResults.js
@@ -1,10 +1,6 @@
 export default function SearchResults({ filteredUser }) {
   return (
-    <li
-      key={filteredUser[0]}
-      className="matched-users"
-      data-userid={filteredUser[0]}
-    >
+    <li className="matched-users" data-userid={filteredUser[0]}>
       {filteredUser[1]}
     </li>
   );
